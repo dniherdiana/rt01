dab7fbe8db25e8d38acbe59af12f9d7c032bad72
dniherdiana <dniherdiana@gmail.com>
2024-05-03 13:48:06 +0700

    .

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,73 +1,7 @@
-/* // import React from "react";
-import { useState } from "react";
-import "./loginForm.css";
-
-export default function LoginForm() {
-  const [userLogin, setUserLogin] = useState({
-    username: "",
-    password: "",
-  });
-
-  const dataConfirmation = (e) => {
-    e.preventDefault();
-    if (
-      userLogin.username === "adminrt_01" &&
-      userLogin.password === "wargart_01"
-    ) {
-      console.log("login berhasil");
-      alert("welcome");
-    } else {
-      alert("ERROR");
-    }
-  };
-
-  const handleInputChange = () => {
-    setUserLogin({ ...userLogin, [e.target.name]: e.target.value });
-  };
-
-  return (
-    <div id="login-form">
-      <form method="post" onSubmit={dataConfirmation}>
-        <fieldset id="form-fieldset">
-          <legend>LOGIN</legend>
-          <label for="username">
-            Username:
-            <input
-              id="username"
-              type="text"
-              name="username"
-              value={setUserLogin.username}
-              onChange={handleInputChange}
-              required
-            />
-          </label>
-          <label for="password">
-            Password:
-            <input
-              id="password"
-              type="password"
-              name="password"
-              value={setUserLogin.password}
-              onChange={handleInputChange}
-            />
-          </label>
-          <button id="login-button" type="submit">
-            Login
-          </button>
-          <div className="login-note">
-            <p> * Untuk username & password silahkan tanyakan ke Pengurus RT</p>
-          </div>
-        </fieldset>
-      </form>
-    </div>
-  );
-}
- */
-
 import React, { useState } from "react";
 import "./loginForm.css";
 
-export default function LoginForm() {
+export default function LoginForm({ onLogin }) {
   const userDetails = {
     username: "adminrt_01",
     password: "wargart_01",
@@ -84,8 +18,7 @@ export default function LoginForm() {
       userLogin.username === userDetails.username &&
       userLogin.password === userDetails.password
     ) {
-      console.log("login berhasil");
-      alert("Welcome!");
+      onLogin(true);
     } else {
       alert("ERROR, password/username not match");
     }
@@ -106,7 +39,7 @@ export default function LoginForm() {
               id="username"
               type="text"
               name="username"
-              value={setUserLogin.username}
+              value={userLogin.username}
               onChange={handleInputChange}
               required
             />
@@ -117,7 +50,7 @@ export default function LoginForm() {
               id="password"
               type="password"
               name="password"
-              value={setUserLogin.password}
+              value={userLogin.password}
               onChange={handleInputChange}
               required
             />
--- src/pages/KeuanganRT/LaporanKeuanganRT/LaporanKeuanganRT.jsx
+++ src/pages/KeuanganRT/LaporanKeuanganRT/LaporanKeuanganRT.jsx
@@ -1,64 +1,49 @@
 import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
+import LoginForm from "../../../components/LoginForm/LoginForm";
 import "./laporanKeuanganRT.css";
 import React, { useState } from "react";
 
 export default function LaporanKeuanganRT() {
   const [iframeLoaded, setIframeLoaded] = useState(false);
-
-  const [hideDisplay, setHideDisplay] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleIframeLoad = () => {
     setIframeLoaded(true);
   };
 
+  const handleLogin = (logginStatus) => {
+    setIsLoggedIn(logginStatus);
+  };
+
   return (
     <div id="laporan-keuangan-rt">
       <Header />
-      <div className="laporan-keuangan-content">
-        <div className="laporan-keuangan-rt">
-          <h1>Laporan Keuangan RT</h1>
+      {!isLoggedIn && <LoginForm onLogin={handleLogin} />}
+      {isLoggedIn && (
+        <div className="laporan-keuangan-content">
+          <div className="laporan-keuangan-rt">
+            <h1>Laporan Keuangan RT</h1>
 
-          <div className="laporan-keuangan">
-            {!iframeLoaded && (
-              <p
-                style={{
-                  fontSize: "1.5rem",
-                  padding: "250px 0",
-                  color: "black",
-                }}
-              >
-                Loading data...
-              </p>
-            )}
+            <div className="laporan-keuangan">
+              {!iframeLoaded && (
+                <p
+                  style={{
+                    fontSize: "1.5rem",
+                    padding: "250px 0",
+                    color: "black",
+                  }}
+                >
+                  Loading data...
+                </p>
+              )}
 
-            <div className="laporan-rt">
-              <iframe
-                width="1250"
-                height="700"
-                referrerPolicy="Access-Control-Allow-Origin"
-                src="https://onedrive.live.com/embed?resid=D746CF77716A5C9E%211483&authkey=%21AMgmAZdUg7vtSP8&em=2&wdAllowInteractivity=False&Item='IPL'!A1%3AV65&wdInConfigurator=True&wdInConfigurator=True"
-                onLoad={handleIframeLoad}
-                style={{
-                  ...{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    margin: "0 auto",
-                  },
-                  display: iframeLoaded ? "flex" : "none",
-                }}
-              ></iframe>
-            </div>
-
-            <div className="laporan-kas">
-              <div className="laporan-card">
+              <div className="laporan-rt">
                 <iframe
-                  width="600"
-                  height="600"
+                  width="1250"
+                  height="700"
                   referrerPolicy="Access-Control-Allow-Origin"
-                  sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-                  src="https://onedrive.live.com/embed?resid=D746CF77716A5C9E%211469&authkey=%21AIJwlthTQDe1qgk&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdInConfigurator=True&wdInConfigurator=True"
+                  src="https://onedrive.live.com/embed?resid=D746CF77716A5C9E%211483&authkey=%21AMgmAZdUg7vtSP8&em=2&wdAllowInteractivity=False&Item='IPL'!A1%3AV65&wdInConfigurator=True&wdInConfigurator=True"
                   onLoad={handleIframeLoad}
                   style={{
                     ...{
@@ -71,28 +56,50 @@ export default function LaporanKeuanganRT() {
                   }}
                 ></iframe>
               </div>
-              <div className="laporan-card">
-                <iframe
-                  width="600"
-                  height="600"
-                  src="https://onedrive.live.com/embed?resid=D746CF77716A5C9E%211486&authkey=%21AJ15A7mpzaQ32sM&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"
-                  onLoad={handleIframeLoad}
-                  style={{
-                    ...{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      margin: "0 auto",
-                    },
-                    display: iframeLoaded ? "flex" : "none",
-                  }}
-                ></iframe>
+
+              <div className="laporan-kas">
+                <div className="laporan-card">
+                  <iframe
+                    width="600"
+                    height="600"
+                    referrerPolicy="Access-Control-Allow-Origin"
+                    sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+                    src="https://onedrive.live.com/embed?resid=D746CF77716A5C9E%211469&authkey=%21AIJwlthTQDe1qgk&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdInConfigurator=True&wdInConfigurator=True"
+                    onLoad={handleIframeLoad}
+                    style={{
+                      ...{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        margin: "0 auto",
+                      },
+                      display: iframeLoaded ? "flex" : "none",
+                    }}
+                  ></iframe>
+                </div>
+                <div className="laporan-card">
+                  <iframe
+                    width="600"
+                    height="600"
+                    src="https://onedrive.live.com/embed?resid=D746CF77716A5C9E%211486&authkey=%21AJ15A7mpzaQ32sM&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"
+                    onLoad={handleIframeLoad}
+                    style={{
+                      ...{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        margin: "0 auto",
+                      },
+                      display: iframeLoaded ? "flex" : "none",
+                    }}
+                  ></iframe>
+                </div>
               </div>
             </div>
           </div>
+          <Footer />
         </div>
-      </div>
-      <Footer />
+      )}
     </div>
   );
 }
