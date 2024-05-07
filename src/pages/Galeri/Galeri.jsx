d0be97c1c757c16782699681d3acf20504257e13
dniherdiana <dniherdiana@gmail.com>
2024-05-07 21:22:14 +0700

    .

--- src/pages/Galeri/Galeri.jsx
+++ src/pages/Galeri/Galeri.jsx
@@ -1,22 +1,15 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-// import UnderConstruction from "../../assets/images/under-construction.jpeg";
+import UnderConstruction from "../../assets/images/under-construction.jpeg";
 import "./galeri.css";
 
 export default function Galeri() {
-  // const imgUrl = "https://imgur.com/V607vyB";
   return (
     <div id="galeri">
       <Header />
-      {/*  <div className="img">
-        <img src={UnderConstruction} alt="" />
-      </div> */}
-
-      <div>
-        <h2>Ronda Ramadhan</h2>
-        <img src={"https://imgur.com/V607vyB"} alt="Ronda Ramadhan" />
-      </div>
-
+      <figure className="img">
+        <img src={UnderConstruction} alt="unavailable" />
+      </figure>
       <Footer />
     </div>
   );
