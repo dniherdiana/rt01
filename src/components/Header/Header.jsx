eca6bbe8e3cd015c594f23a39b19cb199f743ea7
dniherdiana <dniherdiana@gmail.com>
2024-05-07 12:04:01 +0700

    fix height, create burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,59 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
+  const [triggerMenu, isSetTriggerMenu] = useState(false);
+  const handleClickChange = () => {
+    isSetTriggerMenu((open) => !open);
+  };
+
   return (
     <header id="header" className="header-sticky">
-      <div id="header-logo">
-        <h1 className="header-logo">
-          <span className="bg-logo-rt">RT001</span>{" "}
-          <span className="bg-logo-ccr">Cikeas Country Residence</span>
-        </h1>
+      <div className="logo-hideMenu">
+        <div id="header-logo">
+          <h1 className="header-logo">
+            <span className="bg-logo-rt">RT001</span>
+            <span className="bg-logo-ccr">Cikeas Country Residence</span>
+          </h1>
+        </div>
+        <span className="hide-menu" onClick={handleClickChange}>
+          &#9776;
+        </span>
       </div>
 
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to={"/"}>Beranda</Link>
-          </li>
-          <li>
-            <div className="dropdown">
-              <Link to="#">Laporan Keuangan</Link>
-              <div className="dropdown-content">
-                <div className="dropdown-position-rt">
-                  <Link to={"/LaporanKeuanganRT"}>Keuangan RT</Link>
-                </div>
-                <div className="dropdown-position-ruko">
-                  <Link to={"/LaporanKeuanganRuko"}>Keuangan Ruko</Link>
+      <nav className="navbar burger-menu">
+        <div className={`nav-wrapper ${triggerMenu ? "is-open" : ""}`}>
+          <ul>
+            <li>
+              <Link to={"/"}>Beranda</Link>
+            </li>
+            <li>
+              <div className="dropdown">
+                <Link to="#">Laporan Keuangan</Link>
+                <div className="dropdown-content">
+                  <div className="dropdown-position rt">
+                    <Link to={"/LaporanKeuanganRT"}>Keuangan RT</Link>
+                  </div>
+                  <div className="dropdown-position ruko">
+                    <Link to={"/LaporanKeuanganRuko"}>Keuangan Ruko</Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div className="dropdown">
-              <Link to="#">Informasi</Link>
-              <div className="dropdown-content">
-                <Link to={"/StrukturOrganisasi"}>Struktur Organisasi</Link>
-                {/* <Link to="/DataWarga">Data Warga RT01</Link> */}
-                <Link to="/Galeri">Galeri</Link>
+            </li>
+            <li>
+              <div className="dropdown">
+                <Link to="#">Informasi</Link>
+                <div className="dropdown-content">
+                  <Link to={"/StrukturOrganisasi"}>Struktur Organisasi</Link>
+                  <Link to="/Galeri">Galeri</Link>
+                </div>
               </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
--- src/pages/Galeri/Galeri.jsx
+++ src/pages/Galeri/Galeri.jsx
@@ -1,14 +1,23 @@
 import Header from "../../components/Header/Header";
-import UnderConstruction from "../../assets/images/under-construction.jpeg";
+import Footer from "../../components/Footer/Footer";
+// import UnderConstruction from "../../assets/images/under-construction.jpeg";
 import "./galeri.css";
 
 export default function Galeri() {
+  // const imgUrl = "https://imgur.com/V607vyB";
   return (
     <div id="galeri">
       <Header />
-      <div className="img">
+      {/*  <div className="img">
         <img src={UnderConstruction} alt="" />
+      </div> */}
+
+      <div>
+        <h2>Ronda Ramadhan</h2>
+        <img src={"https://imgur.com/V607vyB"} alt="Ronda Ramadhan" />
       </div>
+
+      <Footer />
     </div>
   );
 }
