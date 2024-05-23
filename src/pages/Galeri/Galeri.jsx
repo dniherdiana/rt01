8c6f62b2e44e430744daa10d9ff276c1df679e0d
dniherdiana <dniherdiana@gmail.com>
2024-05-23 20:15:45 +0700

    fix header, footer font-size, add gallery images

--- src/pages/Galeri/Galeri.jsx
+++ src/pages/Galeri/Galeri.jsx
@@ -1,16 +1,41 @@
 import Header from "../../components/Header/Header";
-// import Footer from "../../components/Footer/Footer";
-import UnderConstruction from "../../assets/images/under-construction.jpeg";
+import Footer from "../../components/Footer/Footer";
 import "./galeri.css";
+import rondaRamadhan from "../../db/img/ronda-ramadhan.json";
+import kerjaBaktiRuko from "../../db/img/kerjaBakti-ruko.json";
+import nobarTimnas from "../../db/img/nobar-timnas.json";
+import touringVilla from "../../db/img/touring-villa.json";
 
 export default function Galeri() {
   return (
     <div id="galeri">
       <Header />
+      <div id="gallery-content">
+        <GallerySection
+          title="Kerja bakti area Ruko CCR"
+          images={kerjaBaktiRuko}
+        />
+        <GallerySection title="Ronda Ramadhan" images={rondaRamadhan} />
+        <GallerySection
+          title="Nobar Timnas vs Uzbekistan"
+          images={nobarTimnas}
+        />
+        <GallerySection title="Touring (Villa Diefa)" images={touringVilla} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+function GallerySection({ title, images }) {
+  return (
+    <div className={title.toLowerCase().replace(/ /g, "-")}>
+      <h1 className="title-section">{title}</h1>
       <figure className="img">
-        <img src={UnderConstruction} alt="unavailable" />
+        {images.map((item, index) => (
+          <img key={index} src={item.src} alt={item.alt} />
+        ))}
       </figure>
-      {/* <Footer /> */}
     </div>
   );
 }
