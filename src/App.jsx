f0133582d8c765b291c13b4241245126399b5ebb
dniherdiana <dniherdiana@gmail.com>
2024-09-27 09:49:31 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Beranda from "./pages/Beranda/Beranda";
 import StrukturOrganisasi from "./pages/StrukturOrganisasi/StrukturOrganisasi";
